b9aed9b2de6f07f584ecb6b8b82694f67305c226
Ibrahim <TheawesomeIbz@gmail.com>
2025-04-15 23:46:41 +0100

    added calculator to python on mac

--- Assets/Scriptable Objects/Items/SO_ComplexExponentiationCalculator.cs
+++ Assets/Scriptable Objects/Items/SO_ComplexExponentiationCalculator.cs
@@ -7,11 +7,31 @@ namespace UnityEngine
     [CreateAssetMenu(menuName = "Scriptable Objects/Inventory Items/Complex Root Calculator")]
     public class SO_ComplexExponentiationCalculator : SO_Item, iUsableItem
     {
+        [SerializeField]
+        private string macApplicationPath, macApplicationName, windowsApplicationPath, windowsApplicationName; 
         public void UseItem()
         {
-            if (System.Diagnostics.Process.GetProcessesByName("Complex Root Calculator").Length > 0)
+            switch (Application.platform)
             {
-                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName("Complex Root Calculator");
+                case RuntimePlatform.OSXPlayer:
+                case RuntimePlatform.OSXEditor:
+                    OpenCalculator(macApplicationPath, macApplicationName);
+                    break;
+                case RuntimePlatform.WindowsPlayer:
+                case RuntimePlatform.WindowsEditor:
+                    OpenCalculator(windowsApplicationPath, windowsApplicationName);
+                    break;
+            }
+            
+            
+
+        }
+
+        private void OpenCalculator(string applicationPath, string applicationName)
+        {
+            if (System.Diagnostics.Process.GetProcessesByName(applicationName).Length > 0)
+            {
+                System.Diagnostics.Process[] processes = System.Diagnostics.Process.GetProcessesByName(applicationName);
                 foreach (System.Diagnostics.Process process in processes)
                 {
                     process.Kill();
@@ -19,10 +39,9 @@ namespace UnityEngine
             }
             else
             {
-                string processName = Application.dataPath + "/Scripts/Python Scripts/Complex Root Calculator.exe";
+                string processName = Application.dataPath + applicationPath;
                 System.Diagnostics.Process.Start(processName);
             }
-
         }
-    }
+    }3
 }
